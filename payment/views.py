75d39b2e07d3658389a24914f04e6a18337903cc
Jac <ebrahimi_h2o@yahoo.com>
2023-10-03 18:25:35 +0330

    add some conditions to OrderCreate  when user already paied Show  An Message

--- payment/views.py
+++ payment/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 import requests,json
 from django.shortcuts import render,get_object_or_404,redirect
 from orders.models import Order
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from rest_framework import status
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
@@ -145,38 +145,43 @@ def payment_callback(request):
          return HttpResponse(f'transaction was faild')
 
 
-
+@api_view(['GET'])
 def  payment_process_sandbox(request,order_id):
     order=get_object_or_404(Order,id=order_id)
+    print(order.status)
+    print(order.ORDER_STATUS_PAID)
+    if  order.status!=order.ORDER_STATUS_PAID:
 
-    toman_total_price = order.get_total_price()
-    rial_total_price = toman_total_price*10
+        print(order.status)
 
-    ZarinPal_Request_Url='https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentRequest.json'
-    request_header={
-        'accept':'application/json',
-        'content-type':'application/json'
-    }
-    rial_total_price_float = float(rial_total_price)
-    request_data = {
-        'MerchantID':'abcABCabcABCabcABCabcABCabcABCabcABC',
-        'Amount':rial_total_price_float,
-        'Description':f'#{order.id}:{order.customer.user.first_name} {order.customer.user.last_name}',
-        'CallbackURL':request.build_absolute_uri(reverse('payment:payment_callback')),
+        toman_total_price = order.get_total_price()
+        rial_total_price = toman_total_price*10
 
-    }
+        ZarinPal_Request_Url='https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentRequest.json'
+        request_header={
+            'accept':'application/json',
+            'content-type':'application/json'
+        }
+        rial_total_price_float = float(rial_total_price)
+        request_data = {
+            'MerchantID':'abcABCabcABCabcABCabcABCabcABCabcABC',
+            'Amount':rial_total_price_float,
+            'Description':f'#{order.id}:{order.customer.user.first_name} {order.customer.user.last_name}',
+            'CallbackURL':request.build_absolute_uri(reverse('payment:payment_callback')),
 
-    res=requests.post(ZarinPal_Request_Url,data=json.dumps(request_data),headers=request_header)
-    data=res.json()
-    authority=data['Authority']
-    order.zarinpal_authority=authority
-    order.save()
-    if 'errors' not in data or len(data['errors'])==0 :
-        return redirect('https://sandbox.zarinpal.com/pg/StartPay/{authority}'.format(authority=authority))
+        }
+
+        res=requests.post(ZarinPal_Request_Url,data=json.dumps(request_data),headers=request_header)
+        data=res.json()
+        authority=data['Authority']
+        order.zarinpal_authority=authority
+        order.save()
+        if 'errors' not in data or len(data['errors']) == 0:
+            return redirect(f'https://sandbox.zarinpal.com/pg/StartPay/{authority}')
+        else:
+            return Response({'Success': False, 'Message': 'Error From ZarinPal'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     else:
-        return Response({'Success':False,'Message':'Error From ZarinPal'},status=status.HTTP_500_INTERNAL_SERVER_ERROR),
-
-
+        return Response({'Success': False, 'Message': 'The payment operation of this order has already been successfully completed'}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 # @renderer_classes([JSONRenderer])
